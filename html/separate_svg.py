c03a0f2a8434416bcb435acab6bca6da39b1fb14
Ephron <simondr@belgacom.net>
2022-07-07 00:51:46 +0200

    fix half of page 1

---
+++ html/separate_svg.py
@@ -0,0 +1,19 @@
+
+# %%
+
+with open("page_1/bg_stats.svg", 'rt') as f:
+    lines_to_delete = f.readlines()
+
+# %%
+
+relevant_lines_to_delete = [line for line in lines_to_delete if line.startswith("<path")]
+
+# %%
+
+with open("page_1/bg_other2.svg", 'wt') as outf:
+    with open("page_1/bg_other.svg", 'rt') as inpf:
+        for line in inpf.readlines():
+            if line not in relevant_lines_to_delete:
+                outf.write(line)
+
+# %%

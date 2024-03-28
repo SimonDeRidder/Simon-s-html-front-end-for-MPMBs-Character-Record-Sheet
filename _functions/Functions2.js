c03ac63997e0cf7383fda8eb59cb9b43684c5db3
Ephron <simon.deridder+github@edpnet.be>
2024-03-28 19:19:50 +0100

    v13.1.13 format list in pop-up dialog •	Fix list of known tool/language proficiencies not having proper formatting in the pop-up dialog.

--- _functions/Functions2.js
+++ _functions/Functions2.js
@@ -7932,10 +7932,10 @@ async function AskUserOptions(optType, optSrc, optSubj, knownOpt, notProficienci
 	var diaHeader = notProficiencies ? optType : "Select proficiencies";
 
 	//make all the known options lowercase for easier testing
-	var showOptions = "";
+	var showOptions;
 	if (knownOpt && knownOpt !== "radio") {
-		showOptions = knownOpt.toString();
-		for (var i = 0; i < knownOpt.length; i++) { knownOpt[i] = knownOpt[i].toLowerCase(); };
+		showOptions = formatLineList("", knownOpt);
+		knownOpt = knownOpt.map(function (n) { return n.toLowerCase() });
 	};
 
 	var theDialog = {
